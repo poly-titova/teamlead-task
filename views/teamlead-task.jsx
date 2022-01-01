a1774aac79b166845350bc81395521e56e15312a
Poly Titova <poly_titova@mail.ru>
2022-01-01 12:50:45 +0300

    add teamlead page

---
+++ views/teamlead-task.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Spinner from '@atlaskit/spinner';
+
+export default function TeamleadTask() {
+  return (
+    <>
+      <h1>Teamlead Task</h1>
+      <Spinner />
+    </>
+  );
+}
